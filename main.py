cd742ba4d9cb8efdfe52ffdd9b7d4c7e377afe25
Mary Joyce Nazareno Moico <mamoico@my.cspc.edu.ph>
2026-02-02 15:52:56 +0800

    Enhance main function for web deployment
    Updated main function to support web browser view and dynamic port assignment.

--- main.py
+++ main.py
@@ -85,5 +85,6 @@ def main(page: ft.Page):
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", "8000"))
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER, host="0.0.0.0", port=port)
 
-    ft.app(target=main)
